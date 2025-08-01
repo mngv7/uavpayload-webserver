336976ee4bd9f34ea055939aa41b22cc50af5145
Aron Bakes <aronbakes@gmail.com>
2025-08-01 16:30:10 +1000

    made nav a and nav button use the same base properties, added cute react icons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 import '../assets/Navbar.css';
 import Settings from './Settings';
 
+import {
+  FaTachometerAlt, FaVideo, FaListAlt, FaCog
+} from 'react-icons/fa';
+// FaFlask, FaThermometerHalf, FaTint, FaMapMarkerAlt, FaQrcode, FaDrill, FaDrone,
+
 export function Navbar() {
   const [showSettings, setShowSettings] = useState(false);
 
   return (
     <>
       <nav className="navbar">
-        <a href="/">Dashboard</a>
-        <a href="/live-feed">Live Feed</a>
-        <a href="/logs">Logs</a>
-        <button onClick={() => setShowSettings(!showSettings)}>Settings</button>
+        <a href="/"><FaTachometerAlt />   Dashboard</a>
+        <a href="/live-feed"><FaVideo />   Live Feed</a>
+        <a href="/logs"><FaListAlt />   Logs</a>
+        <button onClick={() => setShowSettings(!showSettings)}><FaCog />   Settings</button>
       </nav>
 
       {showSettings && (
