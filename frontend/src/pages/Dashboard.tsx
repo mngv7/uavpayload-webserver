9cefb6239b419bb17f9d32c78d00b259571e1cbe
mngv7 <zackariya.taylor@gmail.com>
2025-08-12 21:40:24 +1000

    periodic HTTP get request for sensor data every 4 secs

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,49 @@
 import SensorReading from "../components/SensorReading";
 import '../assets/Dashboard.css';
+import { useEffect, useState } from "react";
+import { getSensorReadings } from "../services/api";
 
 function Dashboard() {
+  const [sensorData, setSensorData] = useState(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getSensorReadings();
+        setSensorData(data);
+        setError(null);
+      } catch (err) {
+        setError(String(err));
+      } 
+    }
+
+    fetchData();
+
+    const intervalId = setInterval(fetchData, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  if (error) {
+    return (
+      <>
+        <h1>Dashboard</h1>
+        <h2>Error!</h2>   
+        {error}
+      </>
+    )
+  }
+
+  if (!sensorData) {
+    return (
+      <>
+        <h1>Dashboard</h1>
+        <h2>Loading...</h2>
+      </>
+    )
+  }
+  
   return (
     <div>
       <h1>Dashboard</h1>
